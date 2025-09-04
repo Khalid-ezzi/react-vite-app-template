f09570ac3987afb3d4bf4d63c59db9d45329afbb
Khaled Ezzi <83052530+Khalid-ezzi@users.noreply.github.com>
2025-09-04 18:59:56 -0400

    Add skipErrorHandling option to error service
    Introduces a skipErrorHandling parameter to errorHandlingService, allowing errors to bypass custom handling when needed. Also updates error messages for 404 and 401/403 statuses to include both Arabic and English text for improved clarity.

--- src/services/error-handling-service.tsx
+++ src/services/error-handling-service.tsx
@@ -1,17 +1,22 @@
 import { useErrorStore } from "@/components/context/ErrorContext";
 
-const errorHandlingService = async (error: any, priority: any) => {
-    let errorDetail = error?.response?.data?.detail || error?.response?.data?.error || error?.response?.data || "";
+const errorHandlingService = async (error: any, priority:any, skipErrorHandling?: boolean) => {
+    let errorDetail = error?.response?.data?.detail ||error?.response?.data?.error ||error?.response?.data ||  '';
     let errorMessage = error?.message || "";
     let errorCode = error?.response?.status || "";
 
-    if (error.response?.status === 404) {
+    if(skipErrorHandling) {
+        throw error;
+    }
+    else if (error.response?.status === 404) {
         const { setError } = useErrorStore.getState();
-        setError(errorDetail, "404", "not-found");
+        setError("لم يتم العثور على الصفحة التي تبحث عنها.  \n\n The page you are looking for could not be found.", "404", "not-found");
+        throw error;
     }
     else if (error.response?.status === 401 || error.response?.status === 403) {
         const { setError } = useErrorStore.getState();
-        setError(errorDetail, "401", "not-authorized");
+        setError("ليس لديك الإذن للوصول إلى هذه الصفحة.  \n\n You are not authorized to access this resource.", "401", "not-authorized");
+        throw error;
     }
     else {
         const { setError } = useErrorStore.getState();
